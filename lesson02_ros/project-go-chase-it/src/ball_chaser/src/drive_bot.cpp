573f253f344b6396b8c7a9fb4c788f5cb8c80078
William Nguyen <willhnguyen@gmail.com>
2020-06-24 15:11:06 -0700

    Lesson 2: Project Go Chase It: Change to Publisher Subscriber Class and Custom Robot
    Lesson 2: Project Go Chase It: Move global logic into class contained code
    
    Lesson 2: Project Go Chase It: Remove robot from world
    
    Lesson 2: Project Go Chase It: Modified Robot Design
    
    Update .gitignore to ignore .catkin_workspace and .vscode
    
    Project 2: Project Go Chase It: clean up files

--- lesson02_ros/project-go-chase-it/src/ball_chaser/src/drive_bot.cpp
+++ lesson02_ros/project-go-chase-it/src/ball_chaser/src/drive_bot.cpp
@@ -2,30 +2,48 @@
 #include "geometry_msgs/Twist.h"
 #include "ros/ros.h"
 
-// ROS::Publisher motor commands;
-ros::Publisher motor_command_publisher;
-
-bool handle_drive_request(ball_chaser::DriveToTarget::Request &req, ball_chaser::DriveToTarget::Response &res)
+namespace ball_chaser
 {
-  ROS_INFO("DriveToTargetRequest received - linear_x:%1.2f, angular_z:%1.2f", (float)req.linear_x,
-           (float)req.angular_z);
+class BotDriver
+{
+public:
+  BotDriver()
+  {
+    pub_ = n_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    service_ = n_.advertiseService("/ball_chaser/command_robot", &BotDriver::handle_drive_request, this);
+  }
+  ~BotDriver()
+  {
+  }
 
-  // Create & publish message with requested parameters for linear x and angular z
-  geometry_msgs::Twist motor_command;
-  motor_command.linear.x = req.linear_x;
-  motor_command.angular.z = req.angular_z;
+  bool handle_drive_request(ball_chaser::DriveToTarget::Request &req, ball_chaser::DriveToTarget::Response &res)
+  {
+    ROS_INFO("DriveToTargetRequest received - linear_x:%1.2f, angular_z:%1.2f", (float)req.linear_x,
+             (float)req.angular_z);
 
-  motor_command_publisher.publish(motor_command);
+    // Create & publish message with requested parameters for linear x and angular z
+    geometry_msgs::Twist motor_command;
+    motor_command.linear.x = req.linear_x;
+    motor_command.angular.z = req.angular_z;
 
-  // Give robot time to move
-  ros::Duration(0.5).sleep();
+    pub_.publish(motor_command);
 
-  // Return response message
-  res.msg_feedback = "DriveToTarget set - linear_x: " + std::to_string(motor_command.linear.x) + " , angular_z: " +
-                     std::to_string(motor_command.angular.z);
-  ROS_INFO_STREAM(res.msg_feedback);
+    // Give robot time to move
+    ros::Duration(0.1).sleep();
 
-  return true;
+    // Return response message
+    res.msg_feedback = "DriveToTarget set - linear_x: " + std::to_string(motor_command.linear.x) + " , angular_z: " +
+                       std::to_string(motor_command.angular.z);
+    ROS_INFO_STREAM(res.msg_feedback);
+
+    return true;
+  }
+
+private:
+  ros::NodeHandle n_;
+  ros::Publisher pub_;
+  ros::ServiceServer service_;
+};
 }
 
 int main(int argc, char **argv)
@@ -33,11 +51,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "drive_bot");
   ros::NodeHandle n;
 
-  motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
-
-  ros::ServiceServer drive_service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
-
-  // Handle ROS communication events
+  ball_chaser::BotDriver bot_driver;
   ros::spin();
 
   return 0;
--- lesson02_ros/project-go-chase-it/src/ball_chaser/src/process_image.cpp
+++ lesson02_ros/project-go-chase-it/src/ball_chaser/src/process_image.cpp
@@ -2,115 +2,140 @@
 #include "ball_chaser/DriveToTarget.h"
 #include "ros/ros.h"
 
-ros::ServiceClient client;
-
-void drive_robot(float lin_x, float ang_z)
+namespace ball_chaser
 {
-  ROS_INFO("Moving robot with linear_x %1.2f and angular_z %1.2f", lin_x, ang_z);
-
-  ball_chaser::DriveToTarget srv;
-  srv.request.linear_x = lin_x;
-  srv.request.angular_z = ang_z;
-
-  if (!client.call(srv))
-    ROS_ERROR("Failed to call service command_robot");
-}
-
-inline void drive_right()
+struct BallSearchState
 {
-  ROS_DEBUG_STREAM("Moving robot right");
-  drive_robot(0.0, -0.5);
-}
-inline void drive_left()
-{
-  ROS_DEBUG_STREAM("Moving robot left");
-  drive_robot(0.0, 0.5);
-}
-inline void drive_forward()
-{
-  ROS_DEBUG_STREAM("Moving robot forward");
-  drive_robot(0.5, 0.0);
-}
-inline void drive_stop()
-{
-  ROS_DEBUG_STREAM("Stopping robot");
-  drive_robot(0.0, 0.0);
-}
+  bool found;
+  uint64_t center;
+  uint64_t n_pixels;
+};
 
-void process_image_callback(const sensor_msgs::Image img)
+class Chaser
 {
-  bool ball_found = false;
-  std::vector<uint32_t> white_pixel_x_positions;
-  white_pixel_x_positions.reserve(img.height * img.width);
-
-  // Find x coordinate of center pixel value
-  uint32_t channels = img.step / img.width;
-
-  uint64_t i = 0;
-  for (int h = 0; h < img.height; ++h)
+public:
+  Chaser()
   {
-    for (uint32_t w = 0; w < img.width; ++w)
+    client_ = n_.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
+    sub_ = n_.subscribe("/camera/rgb/image_raw", 10, &Chaser::process_image_callback, this);
+  }
+  ~Chaser()
+  {
+  }
+
+  void process_image_callback(const sensor_msgs::Image img)
+  {
+    const BallSearchState ball_search_state = find_ball(img);
+
+    // Move ball according to ball center x position
+    // Stop ball if not visible or takes up 1/4 of the view
+    uint32_t limit_left = img.width / 3;
+    uint32_t limit_right = limit_left * 2;
+    if (!ball_search_state.found)
+      drive_stop();
+    else if (ball_search_state.center < limit_left)
+      drive_left();
+    else if (ball_search_state.center > limit_right)
+      drive_right();
+    // else if (ball_search_state.n_pixels > ((img.height >> 1) * (img.width >> 1)))
+    //   drive_stop();
+    else
+      drive_forward();
+  }
+
+private:
+  ros::NodeHandle n_;
+  ros::ServiceClient client_;
+  ros::Subscriber sub_;
+
+  void drive_robot(float lin_x, float ang_z)
+  {
+    ROS_INFO("Moving robot with linear_x %1.2f and angular_z %1.2f", lin_x, ang_z);
+
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    if (!client_.call(srv))
+      ROS_ERROR("Failed to call service command_robot");
+  }
+
+  inline void drive_right()
+  {
+    ROS_DEBUG_STREAM("Moving robot right");
+    drive_robot(0.0, -0.5);
+  }
+  inline void drive_left()
+  {
+    ROS_DEBUG_STREAM("Moving robot left");
+    drive_robot(0.0, 0.5);
+  }
+  inline void drive_forward()
+  {
+    ROS_DEBUG_STREAM("Moving robot forward");
+    drive_robot(0.5, 0.0);
+  }
+  inline void drive_stop()
+  {
+    ROS_DEBUG_STREAM("Stopping robot");
+    drive_robot(0.0, 0.0);
+  }
+
+  const BallSearchState find_ball(const sensor_msgs::Image img)
+  {
+    BallSearchState state;
+    state.found = false;
+    state.center = 0;
+
+    std::vector<uint32_t> white_pixel_x_positions;
+    white_pixel_x_positions.reserve(img.height * img.width);
+
+    // Aggregate all white pixel x coordinate values
+    uint32_t channels = img.step / img.width;
+    uint64_t i = 0;
+    for (int h = 0; h < img.height; ++h)
     {
-      // White is when all 3 channels is 255
-      bool is_white = true;
-      for (uint32_t c = 0; c < channels; ++c)
+      for (uint32_t w = 0; w < img.width; ++w)
       {
-        if (img.data[i] != 255)
-          is_white = false;
-        ++i;
-      }
-      if (is_white)
-      {
-        white_pixel_x_positions.push_back(w);
-        ball_found = true;
+        // White is when all 3 channels is 255
+        bool is_white = true;
+        for (uint32_t c = 0; c < channels; ++c)
+        {
+          if (img.data[i] != 255)
+            is_white = false;
+          ++i;
+        }
+        if (is_white)
+        {
+          white_pixel_x_positions.push_back(w);
+          state.found = true;
+        }
       }
     }
-  }
-  ROS_DEBUG_STREAM("Done Processing Image");
+    ROS_DEBUG_STREAM("Done Processing Image");
 
-  // Determine x center
-  uint64_t x_center = 0;
-  uint64_t n_pixels = white_pixel_x_positions.size();
-  if (n_pixels != 0)
-  {
-    for (auto x : white_pixel_x_positions)
-      x_center += x;
-    x_center /= n_pixels;
-  }
-  else
-    ball_found = false;
+    // Determine x center
+    state.n_pixels = white_pixel_x_positions.size();
+    if (state.n_pixels != 0)
+    {
+      for (auto x : white_pixel_x_positions)
+        state.center += x;
+      state.center /= state.n_pixels;
+    }
+    else
+      state.found = false;
 
-  // Move ball according to ball center x position
-  uint32_t limit_left = img.width / 3;
-  uint32_t limit_right = limit_left * 2;
-  if (!ball_found)
-    drive_stop();
-  else if (x_center < limit_left)
-  {
-    drive_left();
+    return state;
   }
-  else if (x_center > limit_right)
-  {
-    drive_right();
-  }
-  // Stop if ball takes up more than half the center view
-  else if (n_pixels > ((img.height >> 1) * (img.width >> 1)))
-    drive_stop();
-  else
-    drive_forward();
+};
 }
-
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "process_image");
   ros::NodeHandle n;
 
-  client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
-
-  ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
-
-  // Handle ROS communication events
+  ball_chaser::Chaser chaser;
   ros::spin();
 
   return 0;
